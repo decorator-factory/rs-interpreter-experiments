a79c4d05080d3d7176fc89d4371d89f6ed4bdb3f
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2023-01-28 01:37:28 +0300

    Fix push-docs script

--- push-docs.sh
+++ push-docs.sh
@@ -2,7 +2,7 @@
 
 set -Eeuo pipefail
 
-cargo doc
+cargo doc --no-deps
 cp -r target/doc ./doc
 echo '<meta http-equiv="refresh" content="0; url=interpreter_experiments">' > doc/index.html
 git add doc/

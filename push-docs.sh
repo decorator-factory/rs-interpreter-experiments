4a79a175648aabbf69ce9f3b5dbce3f379d88706
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2023-01-28 01:29:56 +0300

    Add hacky github pages script

---
+++ push-docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+cargo doc
+cp -r target/doc ./doc
+echo '<meta http-equiv="refresh" content="0; url=interpreter_experiments">' > doc/index.html
+git add doc/
+git commit -m "Docs"
+git push origin `git subtree split --prefix doc master`:gh-pages --force
+git reset --hard HEAD~1
+rm -rf doc
